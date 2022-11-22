563402dbf2a2e400db3deaca4c0eb88465330b42
Jouni Johansson <bgu211@myy.haaga-helia.fi>
2022-11-22 11:03:02 +0200

    first

---
+++ index.js
@@ -0,0 +1,12 @@
+//import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-database.js";
+//const db = getDatabase();
+function laheta() {
+    window.alert("so close");
+   // var teksti = document.getElementById("textikenttä");
+    //var läheta = document.getElementById("lähetä");
+    //set(ref(db), {
+       // viesti: "sometekshe"
+   // });
+}
+
+export { laheta };
